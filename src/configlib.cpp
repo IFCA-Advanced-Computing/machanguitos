9d1d0485050bd3f655684d8a3a48786bb0d831fd
Luis Cabellos <cabellos@ifca.unican.es>
2013-02-19 10:38:41 +0100

    add debug print

--- src/configlib.cpp
+++ src/configlib.cpp
@@ -1,14 +1,29 @@
 #include "configlib.h"
 
+#include <iostream>
+
 #include "lua.hpp"
 
 namespace Config{
 
     int config_add_agent( lua_State *L ){
+        auto name = luaL_checkstring( L, 1 );
+        auto num = luaL_checknumber( L, 2 );
+        std::cout << "Creating: " << num << " of " << name << std::endl;
         return 0;
     }
 
     int config_setvars( lua_State *L ){
+        luaL_checktype( L, 1, LUA_TTABLE );
+        // push first key
+        lua_pushnil( L );
+        while( lua_next( L, 1 ) != 0 ) {
+            std::string keyname( luaL_checkstring( L, -2 ) );
+            std::cout << keyname << " = " 
+                      << lua_typename(L, lua_type(L, -1)) << std::endl;
+            // removes 'value'; keeps 'key' for next iteration
+            lua_pop( L, 1 );
+        }
         return 0;
     }
 
