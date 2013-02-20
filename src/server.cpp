c4719ff316b63e4563f982d2167a590d2e402d1b
Luis Cabellos <cabellos@ifca.unican.es>
2013-02-20 16:16:34 +0100

    use client in server to create agents

--- src/server.cpp
+++ src/server.cpp
@@ -4,19 +4,46 @@
 //------------------------------------------------------------------------------
 #include <iostream>
 
+#include "client.h"
+
 //------------------------------------------------------------------------------
 namespace Engine{
     using namespace std;
 
     //--------------------------------------------------------------------------
-    void Server::addAgents( const string & name, const int n ){
+    void Server::addClient( Client * c ){
+        m_clients.push_back( c );
+    }
+
+    //--------------------------------------------------------------------------
+    void Server::addAgents( const string & name, const unsigned n ){
         m_numAgents[name] = m_numAgents[name] + n;
     }
 
     //--------------------------------------------------------------------------
     void Server::createAgents(){
-        for( auto p: m_numAgents ){
-            cout << "Creating: " << p.second << " of " << p.first << endl;
+        auto nClients = m_clients.size();
+        if( nClients > 0 ){
+            for( const auto p: m_numAgents ){
+                cout << "Creating: " << p.second << " of " << p.first << endl;
+                size_t nPerClient = p.second / nClients;
+                size_t total = nPerClient * nClients;
+
+                if( total < p.second ){
+                    cerr << "WARNING: Creating " << total << " of " << p.second
+                         << " " << p.first << " agents" << endl;
+                }
+
+                for( auto c: m_clients ){
+                    if( c->createClass( p.first ) ){
+                        c->createAgents( p.first, nPerClient );
+                    }else{
+                        cerr << "WARNING: Class " << p.first << " can't be created" << endl;
+                    }
+                }
+            }
+        }else{
+            cerr << "WARNING: No clients" << endl;
         }
     }
 
--- src/server.h
+++ src/server.h
@@ -5,19 +5,24 @@
 //------------------------------------------------------------------------------
 #include <string>
 #include <map>
+#include <list>
 
 #include "singleton.h"
 
 //------------------------------------------------------------------------------
 namespace Engine{
+    class Client;
+
     class Server : public Singleton<Server> {
     public:
-        void addAgents( const std::string & name, const int n );
+        void addClient( Client * c );
+        void addAgents( const std::string & name, const unsigned n );
         void createAgents();
         void run();
 
     private:
-        std::map<std::string, int> m_numAgents;
+        std::map<std::string, unsigned> m_numAgents;
+        std::list<Client*> m_clients;
     };
 }
 
