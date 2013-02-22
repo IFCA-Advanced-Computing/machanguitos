60b6c493036fe861d2520bc8974548a722d5e5ce
Luis Cabellos <cabellos@ifca.unican.es>
2013-02-22 13:32:09 +0100

    x,y test

--- example/cow.lua
+++ example/cow.lua
@@ -1,12 +1,16 @@
 function Agent:init()
    io.write( ">> Agent init\n" )
    self.x = math.random( 10 );
-   io.write( "self.x = " .. self.x .. "\n" )
+   self.y = math.random( 10 );
+   self.n = "hola"
    self:test()
 end
 
 function Agent:update( delta )
    io.write( ">> Agent update = " .. delta .. "\n" )
+   self.x = self.x + 0.1*delta;
+   self.y = self.y + 0.1*delta;
+   io.write( "pos = (" .. self.x .. "," .. self.y .. ")\n" )
 end
 
 function Agent:test()
