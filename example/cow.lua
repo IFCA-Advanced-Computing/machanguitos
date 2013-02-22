3be86870986cae9ae02fcbbbbf44b5c095f12a51
Luis Cabellos <cabellos@ifca.unican.es>
2013-02-22 13:51:31 +0100

    fix cow warn

--- example/cow.lua
+++ example/cow.lua
@@ -2,15 +2,13 @@ function Agent:init()
    io.write( ">> Agent init\n" )
    self.x = math.random( 10 );
    self.y = math.random( 10 );
-   self.n = "hola"
    self:test()
 end
 
 function Agent:update( delta )
-   io.write( ">> Agent update = " .. delta .. "\n" )
    self.x = self.x + 0.1*delta;
    self.y = self.y + 0.1*delta;
-   io.write( "pos = (" .. self.x .. "," .. self.y .. ")\n" )
+   io.write( " cow pos = (" .. self.x .. "," .. self.y .. ")\n" )
 end
 
 function Agent:test()
