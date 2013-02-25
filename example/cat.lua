b2f4ecb17c7b6a99f7d3f0be6cdd5691c91f3e41
Luis Cabellos <cabellos@ifca.unican.es>
2013-02-25 13:43:13 +0100

    randomize cat

--- example/cat.lua
+++ example/cat.lua
@@ -1,7 +1,15 @@
 function Agent:init()
    io.write( "** cat init\n" )
+   self.val = false;
+   self.cname = "micho" .. math.random(100)
 end
 
 function Agent:update( delta )
-   io.write( "** cat update = " .. delta .. "\n" )
+   if self.val then
+      io.write( "** cat " .. self.cname .. " = " .. delta .. " TT \n" )
+   else
+      io.write( "** cat " .. self.cname .. " = " .. delta .. " FF \n" )
+   end
+   self.cname = "micho" .. math.random(100)
+   self.val = not self.val
 end
