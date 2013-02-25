efc942b7849168ed7cef42987742fd92a3449cbf
Luis Cabellos <cabellos@ifca.unican.es>
2013-02-25 12:14:40 +0100

    add string and nil to scriptvalue

--- src/scriptvalue.cpp
+++ src/scriptvalue.cpp
@@ -1,8 +1,106 @@
+//------------------------------------------------------------------------------
 #include "scriptvalue.h"
 
+#include <cassert>
+
+//------------------------------------------------------------------------------
 namespace Util {
-    ScriptValue::ScriptValue() : ScriptValue(0.0) {
+    //--------------------------------------------------------------------------
+    ScriptValue::ScriptValue() : m_type(ValueType::NIL) {
         // empty
     }
-    
+
+    //--------------------------------------------------------------------------
+    ScriptValue::~ScriptValue() {
+        erase();
+    }
+
+    //--------------------------------------------------------------------------
+    void ScriptValue::erase(){
+        if( (m_type == ValueType::STRING) and (m_val.vals != nullptr) ){
+            delete m_val.vals;
+            m_val.vals = nullptr;
+        }
+    }
+
+    //--------------------------------------------------------------------------
+    ScriptValue::ScriptValue( const ScriptValue & val ) : m_type{val.m_type} {
+        switch( m_type ){
+        case ValueType::NIL: // empty
+            break;
+        case ValueType::BOOLEAN:
+            m_val.valb = val.m_val.valb;
+            break;
+        case ValueType::NUMBER:
+            m_val.valn = val.m_val.valn;
+            break;
+        case ValueType::STRING:
+            assert( val.m_val.vals && "Error null string" );
+            m_val.vals = new std::string(*val.m_val.vals);
+            break;
+        }
+    }
+
+    //--------------------------------------------------------------------------
+    ScriptValue::ScriptValue( ScriptValue && val ) : m_type{val.m_type} {
+        switch( m_type ){
+        case ValueType::NIL: // empty
+            break;
+        case ValueType::BOOLEAN:
+            m_val.valb = val.m_val.valb;
+            break;
+        case ValueType::NUMBER:
+            m_val.valn = val.m_val.valn;
+            break;
+        case ValueType::STRING:
+            m_val.vals = std::move(val.m_val.vals);
+            val.m_val.vals = nullptr;
+            break;
+        }
+    }
+
+    //--------------------------------------------------------------------------
+    ScriptValue & ScriptValue::operator=( const ScriptValue & val ){
+        erase();
+        m_type = val.m_type;
+        switch( m_type ){
+        case ValueType::NIL: // empty
+            break;
+        case ValueType::BOOLEAN:
+            m_val.valb = val.m_val.valb;
+            break;
+        case ValueType::NUMBER:
+            m_val.valn = val.m_val.valn;
+            break;
+        case ValueType::STRING:
+            assert( val.m_val.vals && "Error null string" );
+            m_val.vals = new std::string(*val.m_val.vals);
+            break;
+        }
+        return (*this);
+    }
+
+    //--------------------------------------------------------------------------
+    ScriptValue & ScriptValue::operator=( ScriptValue && val ){
+        erase();
+        m_type = val.m_type;
+        switch( m_type ){
+        case ValueType::NIL: // empty
+            break;
+        case ValueType::BOOLEAN:
+            m_val.valb = val.m_val.valb;
+            break;
+        case ValueType::NUMBER:
+            m_val.valn = val.m_val.valn;
+            break;
+        case ValueType::STRING:
+            m_val.vals = std::move(val.m_val.vals);
+            val.m_val.vals = nullptr;
+            break;
+        }
+        return (*this);
+    }
+
 }//namespace Util
+
+//------------------------------------------------------------------------------
--- src/scriptvalue.h
+++ src/scriptvalue.h
@@ -2,42 +2,73 @@
 #ifndef SCRIPTVALUE_H
 #define SCRIPTVALUE_H
 
+#include <iostream>
+#include <string>
+
 namespace Util {
     //--------------------------------------------------------------------------
     class ScriptValue{
     public:
-        enum class ValueType : char { BOOLEAN, NUMBER };
+        enum class ValueType : char { NIL, BOOLEAN, NUMBER, STRING };
 
         ScriptValue();
+        ScriptValue( const ScriptValue & val );
+        ScriptValue( ScriptValue && val );
+        ScriptValue & operator=( const ScriptValue & val );
+        ScriptValue & operator=( ScriptValue && val );
+        ~ScriptValue();
+
         explicit ScriptValue( const bool val );
         explicit ScriptValue( const double val );
+        explicit ScriptValue( const char * val );
+        explicit ScriptValue( const std::string & val );
 
         ValueType getType() const;
 
         bool getBoolean( const bool d = false ) const;
         double getNumber( const double d = 0 ) const;
+        std::string getString( const std::string & s ="" ) const;
 
     private:
-        union{
-            double number;
-            bool tval;
-        } m_val;
+        union Value{
+            double valn;
+            bool valb;
+            std::string * vals;
 
+            Value();
+            ~Value();
+        };
+
+        Value m_val;
         ValueType m_type;
+
+        void erase();
     };
 
     //--------------------------------------------------------------------------
     inline ScriptValue::ScriptValue( const double val ){
-        m_val.number = val;
+        m_val.valn = val;
         m_type = ValueType::NUMBER;
     }
-    
+
     //--------------------------------------------------------------------------
     inline ScriptValue::ScriptValue( const bool val ){
-        m_val.tval = val;
+        m_val.valb = val;
         m_type = ValueType::BOOLEAN;
     }
-    
+
+    //--------------------------------------------------------------------------
+    inline ScriptValue::ScriptValue( const char * val ){
+        m_val.vals = new std::string(val);
+        m_type = ValueType::STRING;
+    }
+
+    //--------------------------------------------------------------------------
+    inline ScriptValue::ScriptValue( const std::string & val ){
+        m_val.vals = new std::string(val);
+        m_type = ValueType::STRING;
+    }
+
     //--------------------------------------------------------------------------
     inline ScriptValue::ValueType ScriptValue::getType() const{
         return m_type;
@@ -45,20 +76,27 @@ namespace Util {
 
     //--------------------------------------------------------------------------
     inline bool ScriptValue::getBoolean( const bool d ) const{
-        if( m_type == ValueType::BOOLEAN ){
-            return m_val.tval;
-        }else{
-            return d;
-        }
+        return ( m_type == ValueType::BOOLEAN ) ? m_val.valb : d;
     }
 
     //--------------------------------------------------------------------------
     inline double ScriptValue::getNumber( const double d ) const{
-        if( m_type == ValueType::NUMBER ){
-            return m_val.number;
-        }else{
-            return d;
-        }
+        return ( m_type == ValueType::NUMBER ) ? m_val.valn : d;
+    }
+
+    //--------------------------------------------------------------------------
+    inline std::string ScriptValue::getString( const std::string & d ) const{
+        return ( m_type == ValueType::STRING ) ? (*m_val.vals) : d;
+    }
+
+    //--------------------------------------------------------------------------
+    inline ScriptValue::Value::Value(){
+        this->vals = nullptr;
+    }
+
+    //--------------------------------------------------------------------------
+    inline ScriptValue::Value::~Value(){
+        // empty
     }
 
 }//namespace Util
