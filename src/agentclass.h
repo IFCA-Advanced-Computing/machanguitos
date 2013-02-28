a68cd2c3c0545806e3f31fdd71a058ee8797f596
Luis Cabellos <cabellos@ifca.unican.es>
2013-02-28 13:22:04 +0100

    document Agent Module

--- src/agentclass.h
+++ src/agentclass.h
@@ -2,26 +2,38 @@
 #ifndef AGENTCLASS_H
 #define AGENTCLASS_H
 
+//------------------------------------------------------------------------------
 #include "lua.hpp"
 #include "agentinstance.h"
 
 //------------------------------------------------------------------------------
 namespace Agent{
     //--------------------------------------------------------------------------
+    /** Agent Table Name for Lua files.
+        @ingroup Agent
+     */
     constexpr const char * SCRIPT_AGENT_NAME = "Agent";
 
     //--------------------------------------------------------------------------
+    /** Class with Class definitions of Agents.
+        @ingroup Agent
+     */
     class AgentClass{
     public:
+        /** Constructs the AgentClass object with a initialiced Lua State.
+            @param L Lua State. It should be already initializated.
+         */
         AgentClass( lua_State * L );
         ~AgentClass();
 
+        /// Create a AgentInstance object of this AgentClass.
         AgentInstance * createInstance();
-
+        /// Returns current Lua State for this AgentClass.
         lua_State * getLua() const;
 
     private:
-        lua_State * m_L{nullptr};
+        /// Lua State
+        lua_State * m_L;
     };
 
     //--------------------------------------------------------------------------
--- src/agentinstance.h
+++ src/agentinstance.h
@@ -8,27 +8,51 @@
 
 #include "scriptvalue.h"
 
+//------------------------------------------------------------------------------
 struct lua_State;
 
 //------------------------------------------------------------------------------
 namespace Agent{
     //--------------------------------------------------------------------------
+    /** private name of AgentInstance objects in Lua tables.
+        @ingroup Agent
+     */
     constexpr const char * SCRIPT_AGENT_OBJ = "__obj";
 
     //--------------------------------------------------------------------------
     class AgentClass;
 
     //--------------------------------------------------------------------------
+    /** Instance of an AgentClass.
+        @ingroup Agent
+     */
     class AgentInstance {
     public:
+        /** AgentInstance constructor. It should receive a AgentClass instance.
+            @param c AgentClass instance of the Agent.
+         */
         AgentInstance( AgentClass * c );
+        /// initialize the Agent calling its 'init' function on Lua.
         void init();
+        /** Update the Agent calling its 'update' fuction on Lua.
+            @param delta time increment in seconds between updates.
+         */
         void update( const double delta );
 
+        /** Put the Agent variable on Lua stack.
+            @param L Lua State.
+            @param key name of the variable to get.
+         */
         int pushData( lua_State * L, const std::string & key ) const;
+        /** Save a Lua value in an Agent variable.
+            @param L Lua State.
+            @param key name of the variable to save.
+         */
         void newData( lua_State * L, const std::string & key );
     private:
-        AgentClass * m_class{nullptr};
+        /// AgentClass instance of this Agent.
+        AgentClass * m_class;
+        /// list of variables of the Agent.
         std::map<std::string, Util::ScriptValue> m_vals;
     };
 }
