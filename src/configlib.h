68e9f5acb50ca9789e47619977cbb602005ef791
Luis Cabellos <cabellos@ifca.unican.es>
2013-03-12 10:03:13 +0100

    add license header to existing code

--- src/configlib.h
+++ src/configlib.h
@@ -1,13 +1,37 @@
+/*******************************************************************************
+Machanguitos is The Easiest Multi-Agent System in the world. Work done at The
+Institute of Physics of Cantabria (IFCA).
+Copyright (C) 2013  Luis Cabellos
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later
+version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+this program.  If not, see <http://www.gnu.org/licenses/>.
+*******************************************************************************/
+//------------------------------------------------------------------------------
 #ifndef CONFIGLIB_H
 #define CONFIGLIB_H
 
+//------------------------------------------------------------------------------
 #include <string>
 
+//------------------------------------------------------------------------------
 struct lua_State;
 
+//------------------------------------------------------------------------------
 namespace Config{
     bool setDataDir( const std::string & filename );
     bool load( const std::string & filename );
 }
 
+//------------------------------------------------------------------------------
 #endif//CONFIGLIB_H
+
+//------------------------------------------------------------------------------
--- src/mpitags.h
+++ src/mpitags.h
@@ -1,7 +1,25 @@
+/*******************************************************************************
+Machanguitos is The Easiest Multi-Agent System in the world. Work done at The
+Institute of Physics of Cantabria (IFCA).
+Copyright (C) 2013  Luis Cabellos
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later
+version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+this program.  If not, see <http://www.gnu.org/licenses/>.
+*******************************************************************************/
 //------------------------------------------------------------------------------
 #ifndef MPITAGS_H
 #define MPITAGS_H
 
+//------------------------------------------------------------------------------
 namespace Engine{
     constexpr int TAG_CREATECLASS = 1;
     constexpr int TAG_CREATEAGENTS = 2;
