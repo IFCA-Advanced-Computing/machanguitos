cc5643ef2a9d5468ccc8b3f25240c7f87248b92e
Luis Cabellos <cabellos@ifca.unican.es>
2013-03-26 13:30:31 +0100

    fix default port of mongo

--- common/datastore.hpp
+++ common/datastore.hpp
@@ -46,7 +46,7 @@ namespace IO {
         /// Default Hostname of Data Store.
         constexpr static const char * const DEFAULT_HOSTNAME{"localhost"};
         /// Default Host port of Data Store.
-        constexpr static uint16_t DEFAULT_PORT{27017};
+        constexpr static uint16_t DEFAULT_PORT{MONGO_DEFAULT_PORT};
 
         DataStore();
         ~DataStore();
