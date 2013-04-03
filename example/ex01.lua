bf19a0f7d1cc4f22710b5e29e206188f95db1ec9
Luis Cabellos <cabellos@ifca.unican.es>
2013-04-03 12:38:24 +0200

    add test of createLayer

--- example/ex01.lua
+++ example/ex01.lua
@@ -1,5 +1,14 @@
 io.write( "Cow Example (Machen " .. config.VERSION_MAJOR .. "." .. config.VERSION_MINOR .. ")\n" );
 
+data.createLayer( {
+                     x0 = 0.0,
+                     x1 = 1.0,
+                     y0 = 0.0,
+                     y1 = 1.0,
+                     w = 100,
+                     h = 100,
+                  } );
+
 config.addAgent( "cow", 10 );
 config.addAgent( "cat", 3 );
 
