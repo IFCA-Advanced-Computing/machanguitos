e38034a42e13b9c9ca41bf8e08aacb801a6b353c
Luis Cabellos <cabellos@ifca.unican.es>
2013-04-11 16:40:34 +0200

    read parameters in create layer

--- machen/datalib.cpp
+++ machen/datalib.cpp
@@ -21,7 +21,11 @@ this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 //------------------------------------------------------------------------------
 #include "datalib.hpp"
+#include <string>
+#include <iostream>
 #include <lua.hpp>
+#include "dataserver.hpp"
+#include "common/util.hpp"
 
 //------------------------------------------------------------------------------
 namespace Data {
@@ -31,11 +35,12 @@ namespace Data {
         This function create a layer in the datastore.
 
         @code{.lua}
-        data.createLayer( {
-                     x0 = 0.0, 
-                     x1 = 1.0, 
-                     y0 = 0.0, 
-                     y1 = 1.0, 
+        data.createLayer( "grass",
+                  {
+                     x0 = 0.0,
+                     x1 = 1.0,
+                     y0 = 0.0,
+                     y1 = 1.0,
                      w = 100,
                      h = 100,
                   } );
@@ -46,6 +51,44 @@ namespace Data {
         @retval 0 No return values to Lua.
      */
     int data_createLayer( lua_State *L ){
+        std::string key = luaL_checkstring( L , 1 );
+
+        int w{ 10 };
+        int h{ 10 };
+        double x0{ 0 }, x1{ 1 }, y0{ 0}, y1{ 1 };
+
+        if( lua_istable( L, 2 ) ){            // 0
+            lua_getfield( L, 2, "w" );        // 1
+            w = luaL_optint( L, -1, w );      // 1
+            lua_pop( L, 1 );                  // 0
+
+            lua_getfield( L, 2, "h" );        // 1
+            h = luaL_optint( L, -1, h );      // 1
+            lua_pop( L, 1 );                  // 0
+
+            lua_getfield( L, 2, "x0" );       // 1
+            x0 = luaL_optnumber( L, -1, x0 ); // 1
+            lua_pop( L, 1 );                  // 0
+
+            lua_getfield( L, 2, "x1" );       // 1
+            x1 = luaL_optnumber( L, -1, x1 ); // 1
+            lua_pop( L, 1 );                  // 0
+
+            lua_getfield( L, 2, "y0" );       // 1
+            y0 = luaL_optnumber( L, -1, y0 ); // 1
+            lua_pop( L, 1 );                  // 0
+
+            lua_getfield( L, 2, "y1" );       // 1
+            y1 = luaL_optnumber( L, -1, y1 ); // 1
+            lua_pop( L, 1 );                  // 0
+        }
+
+        std::cout << "new raster \n";
+        std::cout << "  w = " << w << std::endl;
+        std::cout << "  h = " << h << std::endl;
+        std::cout << "  x = ( " << x0 << ", " << x1 << " )\n";
+        std::cout << "  y = ( " << y0 << ", " << y1 << " )\n";
+
         return 0;
     }
 
