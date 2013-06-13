bb9010d33c8be99ccf20b2f6c7a8201c2a910548
Luis Cabellos <cabellos@ifca.unican.es>
2013-06-13 18:34:19 +0200

    update example to server script

--- example/ex01.lua
+++ example/ex01.lua
@@ -1,24 +1,39 @@
-io.write( "Cow Example (Machen " .. config.VERSION_MAJOR .. "." .. config.VERSION_MINOR .. ")\n" );
+function initialize()
+   io.write( "Cow Example\n" );
+   io.write( " Machen " .. config.VERSION_MAJOR .. "." .. config.VERSION_MINOR .. "\n" );
 
-config.setLogLevel( "debug" );
+   config.setLogLevel( "info" );
 
-data.createLayer( "grass", 
-                  {
-                     x0 = 0.0,
-                     x1 = 20.0,
-                     y0 = 0.0,
-                     y1 = 20.0,
-                     w = 100,
-                     h = 100,
-                  } );
+   data.createLayer( "grass",
+                     {
+                        x0 = 0.0,
+                        x1 = 20.0,
+                        y0 = 0.0,
+                        y1 = 20.0,
+                        w = 100,
+                        h = 100,
+                     } );
 
-config.addAgent( "cow", 10 );
-config.addAgent( "cat", 3 );
+   config.addAgent( "cow", 10 );
+   config.addAgent( "cat", 3 );
 
-config.setvars( {
-                   dbhost = "localhost",
-                   dbport = 27017,
-                   starttime = 0.0,
-                   endtime = 10.0,
-                   iters = 10,
-         } );
+   config.setvars( {
+                      dbhost = "localhost",
+                      dbport = 27017,
+                      starttime = 0.0,
+                      endtime = 10.0,
+                      iters = 10,
+                   } );
+end
+
+function startIteration( num )
+   io.write( "Start iteration " .. num .. "\n" )
+end
+
+function endIteration( num )
+   io.write( "End iteration " .. num .. "\n" )
+end
+
+function endSimulation()
+   io.write( "End Simulation\n" )
+end
