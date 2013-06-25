e0db47a306257801c64d057b6592bb3e6bcd1cfd
Luis Cabellos <cabellos@ifca.unican.es>
2013-06-25 12:26:14 +0200

    add saveRaster hook in data server

--- machen/mpidataserver.cpp
+++ machen/mpidataserver.cpp
@@ -170,6 +170,35 @@ namespace Engine {
     }
 
     //--------------------------------------------------------------------------
+    void runSaveRaster( int src ){
+        char ckey[MAX_CLASS_NAME+1];
+        MPI_Status status;
+        MPI_Recv( &ckey, MAX_CLASS_NAME, MPI_CHAR, src,
+                  MpiTagDS::SAVERASTER, MPI_COMM_WORLD, &status );
+        if( status.MPI_ERROR != MPI_SUCCESS ){
+            LOGE( "Received on data server" );
+            Engine::abort();
+        }
+
+        int count;
+        MPI_Get_count( &status, MPI_CHAR, &count );
+        ckey[count] = 0;
+
+        char cfilename[MAX_PATH_NAME+1];
+        MPI_Recv( &cfilename, MAX_PATH_NAME, MPI_CHAR, src,
+                  MpiTagDS::SAVERASTER, MPI_COMM_WORLD, &status );
+        if( status.MPI_ERROR != MPI_SUCCESS ){
+            LOGE( "Received on data server" );
+            Engine::abort();
+        }
+
+        MPI_Get_count( &status, MPI_CHAR, &count );
+        cfilename[count] = 0;
+
+        LOGI( "Saving raster ", ckey, " -> ", cfilename );
+    }
+
+    //--------------------------------------------------------------------------
     MPIDataServer::MPIDataServer() {
         LOGV( "Creating Data Server ", m_rank );
     }
@@ -191,6 +220,10 @@ namespace Engine {
 
         case MpiTagDS::UPDATERASTERVALUE:
             runUpdateRasterValue( src, val );
+            break;
+
+        case MpiTagDS::SAVERASTER:
+            runSaveRaster( src );
             break;
 
         default:
