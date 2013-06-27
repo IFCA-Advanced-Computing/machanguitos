bd489cda06d5f57f9a603cbf9c3bd1f7537a604d
Luis Cabellos <cabellos@ifca.unican.es>
2013-06-27 13:26:59 +0200

    more random cows

--- example/cow.lua
+++ example/cow.lua
@@ -2,14 +2,16 @@ AgentClass.outVariables( 'x', 'y' )
 
 function Agent:init()
    io.write( ">> Agent init\n" )
-   self.x = math.random( 10 );
-   self.y = math.random( 10 );
+   self.x = 5 + math.random( 10 );
+   self.y = 5 + math.random( 10 );
+   self.dx = 0.2 - math.random( 20 ) / 100.0;
+   self.dy = 0.2 - math.random( 40 ) / 100.0;
    self:test()
 end
 
 function Agent:update( delta )
-   self.x = self.x + 0.1*delta;
-   self.y = self.y + 0.1*delta;
+   self.x = self.x + self.dx*delta;
+   self.y = self.y + self.dy*delta;
    local rst = raster.grass;
    local grass = rst:get( 0, self.x, self.y );
    rst:set( 0, self.x, self.y, grass - 1 );
