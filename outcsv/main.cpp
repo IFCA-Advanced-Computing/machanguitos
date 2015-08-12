d33ff95250d63c7de97b19b0f3c6d1e945133c82
Luis Cabellos <zhen.sydow@gmail.com>
2015-08-12 13:01:09 +0200

    fix documentation files

--- outcsv/main.cpp
+++ outcsv/main.cpp
@@ -15,7 +15,7 @@ PARTICULAR PURPOSE.  See the GNU General Public License for more details.
 You should have received a copy of the GNU General Public License along with
 this program.  If not, see <http://www.gnu.org/licenses/>.
 *******************************************************************************/
-/** @file main.cpp
+/** @file outcsv/main.cpp
     @brief Main function of machen-outcsv application.
     @author Luis Cabellos
 */
